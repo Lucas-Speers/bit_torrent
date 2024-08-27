672776431b2bf76f53e67d49548181202bdf1b3d
Lucas Speers <lucas_speers@outlook.com>
2024-08-26 21:03:10 -0600

    can now read .torrent files

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,83 @@
 #![feature(iter_advance_by)]
 
-use std::{fs::{self, File}, io::Read};
+use std::{env, fmt::Debug, fs, io, process::exit};
 
 /// Bencoding datatypes
-#[derive(Debug)]
+#[derive(PartialEq, Eq)]
 enum DataType {
     Int(i32),
-    Str(String),
+    Str(Vec<u8>),
     List(Vec<DataType>),
-    Dict(Vec<(String, DataType)>),
+    Dict(Vec<(Vec<u8>, DataType)>),
+}
+
+impl Debug for DataType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let DataType::Int(x) = self {
+            write!(f, "Int({x})").unwrap();
+        } else if let DataType::Str(x) = self {
+            write!(f, "Str(\"").unwrap();
+            if x.len() > 100 {
+                write!(f, "OVER 100 CHARACTERS STRING OMITTED").unwrap();
+            } else {
+                for c in x {
+                    write!(f, "{}", *c as char).unwrap();
+                }
+            }
+            write!(f, "\")").unwrap();
+        } else if let DataType::List(x) = self {
+            write!(f, "List(\n").unwrap();
+            for item in x {
+                write!(f, "{item:?}\n").unwrap();
+            }
+            write!(f, "\n)").unwrap();
+        } else if let DataType::Dict(x) = self {
+            write!(f, "Dict(\n").unwrap();
+            for pair in x {
+                // write!(f, "    ").unwrap();
+                for c in &pair.0 {
+                    write!(f, "{}", *c as char).unwrap();
+                }
+                write!(f, ": {:?}\n", pair.1).unwrap();
+            }
+            write!(f, "\n)").unwrap();
+        }
+        Ok(())
+    }
 }
 
 /// returns the number and how many chars it used
-fn decode_int(str: &str, delim: char) -> (i32, usize) {
-    let mut chars = str.chars().enumerate(); // (usize, char)
+fn decode_int(str: &[u8], delim: char) -> (i32, usize) {
+    let mut chars = str.iter().enumerate(); // (usize, char)
     let mut number: i32 = 0;
+    let mut negative = 1;
     loop {
         let char = chars.next().unwrap();
-        if char.1 == delim { return (number, char.0+1); }
-        if char.1 == '-' { number *= -1; }
+        if *char.1 as char == delim { return (number*negative, char.0+1); }
+        if *char.1 as char == '-' { negative = -1 }
         else {
             number *= 10;
-            number += char.1.to_digit(10).unwrap() as i32;
+            number += (char.1 - 48) as i32;
         }
     }
 }
 
 /// returns the string and how many chars it used
-fn decode_str(str: &str) -> (String, usize) {
+fn decode_str(str: &[u8]) -> (Vec<u8>, usize) {
     let int = decode_int(str, ':');
     
-    let mut chars = str.chars();
+    let mut chars = str.iter();
     chars.advance_by(int.1).unwrap();
     
-    let string = chars.take(int.0 as usize).collect::<String>();
+    let string = chars.take(int.0 as usize).map(|x| *x).collect::<Vec<u8>>();
     let len = string.len();
     
     (string, int.1+len)
 }
 
 /// Warning! Does not check the validity of the input
-fn decoder(input_str: &str) -> (DataType, usize) {
-    match input_str.chars().next().unwrap() {
+fn decoder_with_len(input_str: &[u8]) -> (DataType, usize) {
+    match *input_str.iter().next().unwrap() as char {
         'i' => { // integer
             let int = decode_int(&input_str[1..], 'e');
             (DataType::Int(int.0), int.1+1)
@@ -50,8 +86,8 @@ fn decoder(input_str: &str) -> (DataType, usize) {
             let mut output = Vec::new();
             let mut index = 1;
             loop {
-                if input_str.chars().nth(index).unwrap() == 'e' { break; }
-                let value = decoder(&input_str[index..]);
+                if *input_str.iter().nth(index).unwrap() as char == 'e' { break; }
+                let value = decoder_with_len(&input_str[index..]);
                 output.push(value.0);
                 index += value.1;
             }
@@ -62,10 +98,10 @@ fn decoder(input_str: &str) -> (DataType, usize) {
             let mut output = Vec::new();
             let mut index = 1;
             loop {
-                if input_str.chars().nth(index).unwrap() == 'e' { break; }
+                if *input_str.iter().nth(index).unwrap() as char == 'e' { break; }
                 let key = decode_str(&input_str[index..]);
                 index += key.1;
-                let value = decoder(&input_str[index..]);
+                let value = decoder_with_len(&input_str[index..]);
                 output.push((key.0, value.0));
                 index += value.1;
             }
@@ -79,21 +115,40 @@ fn decoder(input_str: &str) -> (DataType, usize) {
     }
 }
 
+fn decoder(input_str: &[u8]) -> DataType {
+    decoder_with_len(input_str).0
+}
+
 fn main() {
-    let file = File::open("archlinux.torrent").unwrap().bytes();
-    // dbg!(decoder(file))
+    let args: Vec<String> = env::args().collect();
+    
+    let mut file_name = String::new();
+    
+    if let Some(x) = args.get(1) {
+        file_name = x.to_string();
+    } else {
+        println!("Enter the name of the .torrent file");
+        io::stdin().read_line(&mut file_name).unwrap();
+        file_name.pop();
+    }
+    
+    println!("Using file \"{file_name}\"");
+    
+    let file = fs::read(file_name).unwrap();
+    println!("{:?}", decoder(&file[..]));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use DataType::*;
 
     #[test]
     fn it_works() {
-        dbg!(decoder("i42e"));
-        dbg!(decoder("i-235e"));
-        dbg!(decoder("12:Hello There!"));
-        dbg!(decoder("l4:spam4:eggsi123ee"));
-        dbg!(decoder("d3:cow3:moo4:spam4:eggse"));
+        assert_eq!(decoder("i42e".as_bytes()), Int(42));
+        assert_eq!(decoder("i-235e".as_bytes()), Int(-235));
+        decoder("12:Hello There!".as_bytes());
+        decoder("l4:spam4:eggsi123ee".as_bytes());
+        decoder("d3:cow3:moo4:spam4:eggse".as_bytes());
     }
 }
